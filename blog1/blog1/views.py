315ad79f3488adf7a59a1426898984b6344dcdae
nishaasan <86878032+nishaasan@users.noreply.github.com>
2024-04-03 06:57:09 +0530

    Delete blog1 directory

--- blog1/blog1/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-def home(request):
-    return HttpResponse("<h1>hello world!!</h1>")
-
-def demo(request):
-    return render(request,'index.html')
